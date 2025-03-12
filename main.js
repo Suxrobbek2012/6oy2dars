edbc7dfa3395024651e1e8000a2b4cdfc1e12793
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-03-13 00:06:19 +0500

    mol

---
+++ main.js
@@ -0,0 +1,239 @@
+function qiyiriq() {
+    alert("Qiyiriq narxi 45000 so'm");
+    const zakaz = +prompt("Qiyiriq nechta zakaz qilasiz?");
+    const narx = 45000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Qiyiriq narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Классик сет narxi 35000 so'm");
+    const zakaz = +prompt("Классик сет nechta zakaz qilasiz?");
+    const narx = 35000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Классик сет narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function longer() {
+    alert("Лонгер рингс сет narxi 45000 so'm");
+    const zakaz = +prompt("Лонгер рингс сет nechta zakaz qilasiz?");
+    const narx = 45000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Лонгер рингс сет narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}function qiyiriq() {
+    alert("Биг сет narxi 62000 so'm");
+    const zakaz = +prompt("Биг сет nechta zakaz qilasiz?");
+    const narx = 62000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Биг сет narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Лестер narxi 53000 so'm");
+    const zakaz = +prompt("Лестер nechta zakaz qilasiz?");
+    const narx = 53000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Лестер narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Скул сет narxi 36000 so'm");
+    const zakaz = +prompt("Скул сет nechta zakaz qilasiz?");
+    const narx = 36000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Скул сет narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Комбо сет Большой narxi 20000 so'm");
+    const zakaz = +prompt("Комбо сет Большой nechta zakaz qilasiz?");
+    const narx = 20000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Комбо сет Большой narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Do’stlar 1х narxi 43000 so'm");
+    const zakaz = +prompt("Do’stlar 1х nechta zakaz qilasiz?");
+    const narx = 43000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Do’stlar 1х narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Do’stlar 2х narxi 79000 so'm");
+    const zakaz = +prompt("Do’stlar 2х nechta zakaz qilasiz?");
+    const narx = 79000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Do’stlar 2х narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Do’stlar 4х narxi 145000 so'm");
+    const zakaz = +prompt("Do’stlar 4х nechta zakaz qilasiz?");
+    const narx = 145000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Do’stlar 4х narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Острый Do’stlar 1х narxi 45000 so'm");
+    const zakaz = +prompt("Острый Do’stlar 1х nechta zakaz qilasiz?");
+    const narx = 45000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Острый Do’stlar 1х narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
+function qiyiriq() {
+    alert("Qiyqiriq narxi 43000 so'm");
+    const zakaz = +prompt("Qiyqiriq nechta zakaz qilasiz?");
+    const narx = 43000;
+
+    if (!isNaN(zakaz) && zakaz > 0) {
+        alert(`${zakaz} ta Qiyqiriq narxi ${zakaz * narx} so'm bo'ladi`);
+        const savol = confirm("Zakaz qilasizmi? ");
+        switch(savol){
+            case true:
+                alert("Zakaz qabul qilindi")
+                break;
+                default:
+                    alert("Zakaz bekor qilindi")
+                    break;
+        }
+    } else {
+        alert("Iltimos, to‘g‘ri son kiriting!");
+    }
+}
